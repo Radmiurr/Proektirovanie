bdaa23fab7a199e908dff236f4d60b1dd4e15d93
Radmiurr <124788354+Radmiurr@users.noreply.github.com>
2024-04-20 01:08:56 +0600

    Add files via upload

---
+++ Lab_4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleApp1;
+
+namespace console.app
+{
+    public class Employee
+    {
+        public int Id;
+        public string Name;
+        public string Position;
+        public decimal Salary;
+        public int HoursWorked;
+        public int Seniority;
+    }
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            List<Employee> employees = SqlManager.GetEmployees();
+
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine($"Имя: {employee.Name}");
+                Console.WriteLine($"Должность: {employee.Position}");
+                Console.WriteLine($"Зарплата: {employee.Salary}");
+                Console.WriteLine($"Отработанные часы в месяце: {employee.HoursWorked}");
+                Console.WriteLine($"Стаж: {employee.Seniority}");
+                if (employee.Position == "Менеджер"){
+                    int bonus = 300;
+                    decimal salary = (employee.Salary + bonus) * employee.Seniority;
+                    Console.WriteLine($"Заработная плата за месяц: {salary}");
+                }
+                if (employee.Position == "Повар")
+                {
+                    decimal salary = employee.Salary * employee.HoursWorked;
+                    Console.WriteLine($"Заработная плата за месяц: {salary}");
+                }
+                if (employee.Position == "Официант")
+                {
+                    int tips = 150;
+                    decimal salary = (employee.Salary * employee.HoursWorked) + tips;
+                    Console.WriteLine($"Заработная плата за месяц: {salary}");
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
---
+++ Lab_4/SqlManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using console.app;
+
+
+namespace ConsoleApp1
+{
+    public class SqlManager
+    {
+        private static string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=usersdb;Integrated Security=True";
+
+
+            public static List<Employee> GetEmployees()
+            {
+                List<Employee> employees = new List<Employee>();
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand("SELECT * FROM Employee", connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                employees.Add(new Employee
+                                {
+                                    Id = reader.GetInt32(0),
+                                    Name = reader.GetString(1),
+                                    Position = reader.GetString(2),
+                                    Salary = reader.GetDecimal(3),
+                                    HoursWorked = reader.GetInt32(4),
+                                    Seniority = reader.GetInt32(5)
+                                });
+                            }
+                        }
+                    }
+
+                    connection.Close();
+                }
+
+                return employees;
+            }
+        
+    }
+}
